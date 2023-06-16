d45d1e0fc7bcabd07702249a9274a38afe6ab7cf
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2023-06-16 23:05:59 +1000

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+//base URL; directs to API
+const BASE_URL = 'https://api.themoviedb.org/3'; 
+//authenticate requests to API
+const API_KEY = 'api_key=6a8725217517f3fc5184d578b96e25ec'; 
+
+/**Methods for working search bar */
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const SEARCH_URL = BASE_URL + '/search/movie?' + API_KEY;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value;
+
+    if(searchTerm) {
+        getMovies(SEARCH_URL+'&query='+searchTerm);
+    } else {
+        getMovies(GET_MOVIES_URL);
+    }
+})
+
+
+/**Methods for getting popular movies */
+    //API endpoint, traverses API to find endpoint that will execute an action
+const GET_MOVIES_URL = BASE_URL + '/discover/movie?sort_by=popularity.desc&' + API_KEY; 
+
+const IMAGE_URL = 'https://image.tmdb.org/t/p/w500/';
+const main = document.getElementById('main');
+
+getMovies(GET_MOVIES_URL);
+
+function getMovies(url) {
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        showMovies(data.results); //array of elements
+    })
+}
+//loops through array of data, for each element/movie, create a html card from the index.html
+function showMovies(data) {
+    main.innerHTML = '';
+
+    data.forEach(movie => {
+        // const {title, poster_path, vote_average, overview} = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+        <img src="${IMAGE_URL + movie.poster_path}" alt="${movie.title}">
+
+            <div class="movie-info">
+                <h3>"${movie.title}"</h3>
+                <span class="${getColor(movie.vote_average)}">${movie.vote_average}</span>
+            </div> 
+
+            <div class="overview">
+                <h3>Overview</h3>
+                "${movie.overview}"               
+            </div>
+        
+        `
+        main.appendChild(movieEl)
+    });
+}
+
+function getColor(vote_ave){
+    if(vote_ave >= 7.5){return 'green';}
+    else if (vote_ave < 7.5 && vote_ave >= 5) {return 'orange';}
+    else {return 'red';}
+}
