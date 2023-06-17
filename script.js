e5861b666e50d2f145923a39bf4d386fb10f0edc
JuanTwo779 <104906032+JuanTwo779@users.noreply.github.com>
2023-06-17 17:51:12 +1000

    pagination

--- script.js
+++ script.js
@@ -3,6 +3,48 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 //authenticate requests to API
 const API_KEY = 'api_key=6a8725217517f3fc5184d578b96e25ec'; 
 
+
+/**Methods for pagination */
+const prev = document.getElementById('previous');
+const next = document.getElementById('next');
+const current = document.getElementById('current');
+
+var currentPage = 1;
+var nextPage = 2;
+var prevPage = 3;
+var lastUrl = '';
+var totalPages = 100;
+
+next.addEventListener('click', () => {
+    if(nextPage <= totalPages) {
+        pageCall(nextPage)
+    }
+})
+
+function pageCall(page) {
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length - 1].split('=');
+    if(key[0] != 'page'){
+        let url = lastUrl + '&page=' + page;
+        getMovies(url);
+    } else {
+        key[1] = page.toString();
+        let a = key.join('=');
+        queryParams[queryParams.length -1] = a;
+        let b = queryParams.join('&');
+        let url = urlSplit[0] + '?' + b;
+        getMovies(url);
+    }
+}
+
+prev.addEventListener('click', () => {
+    if(prevPage > 0) {
+        pageCall(prevPage);
+    }
+})
+
+
 /**Methods for working search bar */
 const form = document.getElementById('form');
 const search = document.getElementById('search');
@@ -32,14 +74,42 @@ getMovies(GET_MOVIES_URL);
 
 function getMovies(url) {
 
+    lastUrl = url;
+
     fetch(url)
     .then(res => res.json())
     .then(data => {
         console.log(data);
-        showMovies(data.results); //array of elements
+        
+        if(data.results.length !== 0){
+            showMovies(data.results); //array of elements
+            currentPage = data.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = data.total_pages;
+
+            current.innerText = currentPage;
+
+            if (currentPage <= 1) {
+                prev.classList.add('disabled');
+                next.classList.remove('disabled');
+            }else if (currentPage >= totalPages) {
+                next.classList.add('disabled');
+                prev.classList.remove('disabled');
+            }else {
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled');
+            }
+
+            search.scrollIntoView({behavior : "smooth"});
+
+        }else{
+            main.innerHTML= `<h1 class="no-results">No Results Found</h1>`
+        }
+
     })
 }
-//loops through array of data, for each element/movie, create a html card from the index.html
+    //loops through array of data, for each element/movie, create a html card from the index.html
 function showMovies(data) {
     main.innerHTML = '';
 
